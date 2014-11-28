d29ff7459796a94400e547c2964e806fcc1a0beb
Bervyn Co <bervyn_co2010@yahoo.com>
2014-11-28 10:40:15 +0800

    Copied files from local to Git

---
+++ fundfact.js
@@ -0,0 +1,33 @@
+function fundFactPanelController($scope)
+{
+	$scope.funds = [
+		{id:1, fundName:'Wealth Bond Fund'},
+		{id:2, fundName:'Wealth Balanced Fund'},
+		{id:3, fundName:'Wealth Equity Fund'},
+		{id:4, fundName:'Liquidity Fund'},
+		{id:5, fundName:'Opportunity Fund'},
+		{id:6, fundName:'Chinese Tycoon Fund'},
+		{id:7, fundName:'Spanish American Legacy Fund'},
+		{id:8, fundName:'Premium Bond Fund'},
+		{id:9, fundName:'Capital Investment Fund'},
+		{id:10, fundName:'Global Advantage Fund'},
+		{id:11, fundName:'European Wealth Fund'},
+		{id:12, fundName:'Asia Growth Fund'},
+		{id:13, fundName:'3G-Xceed Dollar B19 Fund'},
+		{id:14, fundName:'3G-Xceed Dollar B20 Fund'},
+		{id:15, fundName:'3G-Xceed Dollar B21fund'},
+		{id:16, fundName:'3G-Xceed B16 Fund'},
+		{id:17, fundName:'3G-Xceed B16 Fund Series 2'},
+		{id:18, fundName:'3G-Xceed B17 Fund'},
+		{id:19, fundName:'3G-Xceed B17 Fund Series 2'},
+		{id:20, fundName:'3G-Xceed B18 Fund'},
+		{id:21, fundName:'3G-Xceed B18 Fund Series 2'},
+		{id:22, fundName:'3G-Xceed B19 Fund'},
+		{id:23, fundName:'3G-Xceed B20 Fund'},
+		{id:24, fundName:'3G-Xceed B21 Fund'},
+		{id:21, fundName:'Dollar Phoenix 5 Fund'},
+		{id:22, fundName:'Dollar Phoenix 7 Fund'},
+		{id:23, fundName:'Peso Phoenix 5 Fund'},
+		{id:24, fundName:'Peso Phoenix 7 Fund'}
+];
+}
