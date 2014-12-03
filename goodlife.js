4ec553af9396198774d0039bb234da94b2054428
Bervyn Co <bervyn_co2010@yahoo.com>
2014-12-03 13:31:00 +0800

    In progress: contact.html

--- goodlife.js
+++ goodlife.js
@@ -1,5 +1,5 @@
 var fundFactApp = angular.module('fundFactApp', []);
-
+var contactApp = angular.module('ContactApp', []);
 fundFactApp.factory('fundFactory', function() {
   return {
     getFundNames: function() {
@@ -51,4 +51,55 @@ $(document).ready(function() {
     );
 }); // end ready
 
-fundFactApp.
+//Angular App Module and Controller
+
+14.600211, 120.974237
+contactApp.controller('MapController', function ($scope) {
+var link = function(scope, element, attrs) {
+        var map, infoWindow;
+        var markers = [];
+        
+        // map config
+        var mapOptions = {
+            center: new google.maps.LatLng(50, 2),
+            zoom: 4,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            scrollwheel: false
+        };
+        
+        // init the map
+        function initMap() {
+            if (map === void 0) {
+                map = new google.maps.Map(element[0], mapOptions);
+            }
+        }    
+        
+        // place a marker
+        function setMarker(map, position, title, content) {
+            var marker;
+            var markerOptions = {
+                position: position,
+                map: map,
+                title: title,
+                icon: 'https://maps.google.com/mapfiles/ms/icons/green-dot.png'
+            };
+
+            marker = new google.maps.Marker(markerOptions);
+            markers.push(marker); // add marker to array
+        }
+        
+        // show the map and place some markers
+        initMap();
+        
+        setMarker(map, new google.maps.LatLng(51.508515, -0.125487), 'London', 'Just some content');
+        setMarker(map, new google.maps.LatLng(52.370216, 4.895168), 'Amsterdam', 'More content');
+        setMarker(map, new google.maps.LatLng(48.856614, 2.352222), 'Paris', 'Text here');
+    };
+    
+    return {
+        restrict: 'A',
+        template: '<div id="map"></div>',
+        replace: true,
+        link: link
+    };
+});
