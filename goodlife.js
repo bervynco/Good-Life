4ae30e9ae952ffdf610ac5f6ae20db4b4f05ab6a
Bervyn Co <bervyn_co2010@yahoo.com>
2014-12-10 17:11:07 +0800

    Maps done

--- goodlife.js
+++ goodlife.js
@@ -1,5 +1,4 @@
 var fundFactApp = angular.module('fundFactApp', []);
-var contactApp = angular.module('ContactApp', []);
 fundFactApp.factory('fundFactory', function() {
   return {
     getFundNames: function() {
@@ -54,52 +53,18 @@ $(document).ready(function() {
 //Angular App Module and Controller
 
 //14.600211, 120.974237
-contactApp.controller('MapController', function ($scope) {
-var link = function(scope, element, attrs) {
-        var map, infoWindow;
-        var markers = [];
-        
-        // map config
-        var mapOptions = {
-            center: new google.maps.LatLng(50, 2),
-            zoom: 4,
-            mapTypeId: google.maps.MapTypeId.ROADMAP,
-            scrollwheel: false
-        };
-        
-        // init the map
-        function initMap() {
-            if (map === void 0) {
-                map = new google.maps.Map(element[0], mapOptions);
-            }
-        }    
-        
-        // place a marker
-        function setMarker(map, position, title, content) {
-            var marker;
-            var markerOptions = {
-                position: position,
-                map: map,
-                title: title,
-                icon: 'https://maps.google.com/mapfiles/ms/icons/green-dot.png'
-            };
+angular.module('ContactApp', ['uiGmapgoogle-maps'])
+  .controller('MapCtrl', function ($scope, $log) {
 
-            marker = new google.maps.Marker(markerOptions);
-            markers.push(marker); // add marker to array
-        }
-        
-        // show the map and place some markers
-        initMap();
-        
-        setMarker(map, new google.maps.LatLng(51.508515, -0.125487), 'London', 'Just some content');
-        setMarker(map, new google.maps.LatLng(52.370216, 4.895168), 'Amsterdam', 'More content');
-        setMarker(map, new google.maps.LatLng(48.856614, 2.352222), 'Paris', 'Text here');
-    };
-    
-    return {
-        restrict: 'A',
-        template: '<div id="map"></div>',
-        replace: true,
-        link: link
-    };
-});
+    $scope.map = {center: {latitude: 14.600211, longitude: 120.974237 }, zoom: 18 };
+    $scope.options = {scrollwheel: false};
+    $scope.markers = [{
+      id: 0,
+      coords: {
+        latitude: 14.600211,
+        longitude: 120.974237
+      },
+      options: { draggable: true },
+    }];
+      console.log($scope.markers);
+  });
